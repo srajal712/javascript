401074214abf4e4f4d4850d4c20317f175e84fb5
srajal712 <77794346+srajal712@users.noreply.github.com>
2023-12-30 06:24:25 +0000

    commit

--- basics/03_conversionOperation.js
+++ basics/03_conversionOperation.js
@@ -22,6 +22,40 @@ let booleanIsLoggedIn =Boolean(isLoggedIn)
 
 let someNumber = 33 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
- 
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ***** Operations ****
+
+let value = 3
+let negVal = -value
+// console.log(negVal);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2%2);
+
+let str1= "hello"
+let str2= " srajal"
+let str3= str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+console.log(true); 
+console.log(+true);
+// console.log(true+); // error
+
+let num1 , num2 , num3
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter);
+
+  
+
